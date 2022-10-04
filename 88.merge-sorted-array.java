38516dc8da3fe1ce2ab9acbf464d1f76b5d330ce
Sampad <realsampaddey@gmail.com>
2022-10-04 18:16:44 +0530

    88.merge-sorted-array.java

--- 88.merge-sorted-array.java
+++ 88.merge-sorted-array.java
@@ -1,28 +1,16 @@
-/*
- * @lc app=leetcode id=88 lang=java
- *
- * [88] Merge Sorted Array
- */
-
-// @lc code=start
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int insertIndex = m + n - 1;
-        int indexA = m - 1, indexB = n - 1;
-
-        while (indexB >= 0) {
-            if (indexA < 0) {
-                nums1[insertIndex--] = nums2[indexB--];
+        int i = m - 1;
+        int j = n - 1;
+        while (j >= 0 && i >= 0) {
+            if (nums2[j] >= nums1[i]) {
+                nums1[j + i + 1] = nums2[j--];
             } else {
-                if (nums2[indexB] >= nums1[indexA]) {
-                    nums1[insertIndex--] = nums2[indexB--];
-                } else {
-                    nums1[insertIndex--] = nums1[indexA--];
-                }
+                nums1[j + i + 1] = nums1[i--];
             }
+        }
+        while (j >= 0) {
+            nums1[i + j + 1] = nums2[j--];
         }
     }
 }
-
-// @lc code=end
-
