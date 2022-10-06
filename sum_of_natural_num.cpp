18b0743c8f9c9cb979bbfaf472469e71937f0eda
Naman Katariya <109310617+katariyanaman@users.noreply.github.com>
2022-10-06 22:55:35 +0530

    Create sum_of_natural_num.cpp

---
+++ sum_of_natural_num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n,sum;
+
+    cout<<"Enter the number of terms : ";
+    cin>>n;
+
+    sum=n*(n+1)/2;
+
+    cout<<"The sum is : "<<sum<<endl;
+    return 0;
+}
